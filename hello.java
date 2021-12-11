15e259245714097f57040db182e94717cbdf908a
Nitya <azp4651@gmail.com>
2021-12-11 20:53:38 +0530

    hello program created

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello{
+
+public static void main(String [] args){
+     //single line comment
+	 /*multi line comment
+	    n number of line
+     */
+	 /**
+	 java doc comment
+	 */
+	 System.out.println("hello");
+}
+	 }
